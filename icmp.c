09e7ea732deaa11fbbacd8df456c45e87a716c25
ShmuelLa <shmuel516@gmail.com>
2021-01-16 00:51:02 +0200

    removed IP HDR

--- icmp.c
+++ icmp.c
@@ -1,6 +1,5 @@
 // icmp.cpp
 // Sending ICMP Echo Requests using Raw-sockets.
-
 #include <stdio.h>
 
 #if defined _WIN32
@@ -19,171 +18,68 @@
 #include <sys/time.h>
 #endif
 
-// IPv4 header len without options
-#define IP4_HDRLEN 20
-
 // ICMP header len for echo req
 #define ICMP_HDRLEN 8 
-
 // Checksum algo
 unsigned short calculate_checksum(unsigned short * paddress, int len);
 
 // 1. Change SOURCE_IP and DESTINATION_IP to the relevant for your computer
 // 2. Compile it using MSVC compiler or g++
 // 3. Run it from the account with administrative permissions,
-//    since opening of a raw-socket requires elevated preveledges.
-//
-//    On Linux, run it as a root or with sudo.
-//
+//    since opening of a raw-socket requires elevated preveledges
 // 4. For debugging and development, run MS Visual Studio (MSVS) as admin by
 //    right-clicking at the icon of MSVS and selecting from the right-click 
 //    menu "Run as administrator"
-//
 //  Note. You can place another IP-source address that does not belong to your
 //  computer (IP-spoofing), i.e. just another IP from your subnet, and the ICMP
 //  still be sent, but do not expect to see ICMP_ECHO_REPLY in most such cases
 //  since anti-spoofing is wide-spread.
 
 #define SOURCE_IP "172.19.93.135"
-// i.e the gateway or ping to google.com for their ip-address
 #define DESTINATION_IP "8.8.8.8"
 
-int main ()
-{
-    struct ip iphdr; // IPv4 header
+int main () {
     struct icmp icmphdr; // ICMP-header
     char data[IP_MAXPACKET] = "This is the ping.\n";
-
     int datalen = strlen(data) + 1;
-
-    //==================
-    // IP header
-    //==================
-
-    // IP protocol version (4 bits)
-    iphdr.ip_v = 4;
-
-    // IP header length (4 bits): Number of 32-bit words in header = 5
-    iphdr.ip_hl = IP4_HDRLEN / 4; // not the most correct
-
-    // Type of service (8 bits) - not using, zero it.
-    iphdr.ip_tos = 0;
-
-    // Total length of datagram (16 bits): IP header + ICMP header + ICMP data
-    iphdr.ip_len = htons (IP4_HDRLEN + ICMP_HDRLEN + datalen);
-
-    // ID sequence number (16 bits): not in use since we do not allow fragmentation
-    iphdr.ip_id = 0;
-
-    // Fragmentation bits - we are sending short packets below MTU-size and without 
-    // fragmentation
-    int ip_flags[4];
-
-    // Reserved bit
-    ip_flags[0] = 0;
-
-    // "Do not fragment" bit
-    ip_flags[1] = 0;
-
-    // "More fragments" bit
-    ip_flags[2] = 0;
-
-    // Fragmentation offset (13 bits)
-    ip_flags[3] = 0;
-
-    iphdr.ip_off = htons ((ip_flags[0] << 15) + (ip_flags[1] << 14)
-                      + (ip_flags[2] << 13) +  ip_flags[3]);
-
-    // TTL (8 bits): 128 - you can play with it: set to some reasonable number
-    iphdr.ip_ttl = 128;
-
-    // Upper protocol (8 bits): ICMP is protocol number 1
-    iphdr.ip_p = IPPROTO_ICMP;
-
-    // Source IP
-    if (inet_pton (AF_INET, SOURCE_IP, &(iphdr.ip_src)) <= 0) 
-    {
-        fprintf (stderr, "inet_pton() failed for source-ip with error: %d"
-#if defined _WIN32
-			, WSAGetLastError()
-#else
-			, errno
-#endif
-			);
-        return -1;
-    }
-    // Destination IPv
-    if (inet_pton (AF_INET, DESTINATION_IP, &(iphdr.ip_dst)) <= 0)
-    {
-        fprintf (stderr, "inet_pton() failed for destination-ip with error: %d" 
-#if defined _WIN32
-			, WSAGetLastError()
-#else
-			, errno
-#endif
-			);
-        return -1;
-    }
-    // IPv4 header checksum (16 bits): set to 0 prior to calculating in order not to include itself.
-    iphdr.ip_sum = 0;
-    iphdr.ip_sum = calculate_checksum((unsigned short *) &iphdr, IP4_HDRLEN);
-
     //===================
     // ICMP header
     //===================
 
     // Message Type (8 bits): ICMP_ECHO_REQUEST
     icmphdr.icmp_type = ICMP_ECHO;
-
     // Message Code (8 bits): echo request
     icmphdr.icmp_code = 0;
-
     // Identifier (16 bits): some number to trace the response.
     // It will be copied to the response packet and used to map response to the request sent earlier.
     // Thus, it serves as a Transaction-ID when we need to make "ping"
     icmphdr.icmp_id = 18; // hai
-
     // Sequence Number (16 bits): starts at 0
     icmphdr.icmp_seq = 0;
-
     // ICMP header checksum (16 bits): set to 0 not to include into checksum calculation
     icmphdr.icmp_cksum = 0;
-
     // Combine the packet 
     char packet[IP_MAXPACKET];
 
-    // First, IP header.
-    memcpy (packet, &iphdr, IP4_HDRLEN);
-
     // Next, ICMP header
-    memcpy ((packet + IP4_HDRLEN), &icmphdr, ICMP_HDRLEN);
-
+    memcpy ((packet), &icmphdr, ICMP_HDRLEN);
     // After ICMP header, add the ICMP data.
-    memcpy (packet + IP4_HDRLEN + ICMP_HDRLEN, data, datalen);
-
+    memcpy (packet + ICMP_HDRLEN, data, datalen);
     // Calculate the ICMP header checksum
-    icmphdr.icmp_cksum = calculate_checksum((unsigned short *) (packet + IP4_HDRLEN), ICMP_HDRLEN + datalen);
-    memcpy ((packet + IP4_HDRLEN), &icmphdr, ICMP_HDRLEN);
-
+    icmphdr.icmp_cksum = calculate_checksum((unsigned short *) (packet), ICMP_HDRLEN + datalen);
+    memcpy ((packet), &icmphdr, ICMP_HDRLEN);
     struct sockaddr_in dest_in;
     memset (&dest_in, 0, sizeof (struct sockaddr_in));
     dest_in.sin_family = AF_INET;
-
-    // The port is irrelant for Networking and therefore was zeroed.
-#if defined _WIN32
-#else
-    dest_in.sin_addr.s_addr = iphdr.ip_dst.s_addr;
-#endif
-    
-#if defined _WIN32
-#endif
     // Create raw socket for IP-RAW (make IP-header by yourself)
+    //
+    // Here we changed it to IPPROTO_ICMP to tell the OS that we wan't to only create an ICMP header
+    //
     int sock = -1;
     if ((sock = socket (AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1) 
     {
         fprintf (stderr, "socket() failed with error: %d"
 #if defined _WIN32
-			, WSAGetLastError()
 #else
 			, errno
 #endif
@@ -194,37 +90,17 @@ int main ()
     // This socket option IP_HDRINCL says that we are building IPv4 header by ourselves, and
     // the networking in kernel is in charge only for Ethernet header.
     const int flagOne = 1;
-    if (setsockopt (sock, IPPROTO_IP, IP_HDRINCL,
-#if defined _WIN32
-#endif
-		&flagOne, // The above casting is important for Windows.
-		sizeof (flagOne)) == -1) 
-    {
-        fprintf (stderr, "setsockopt() failed with error: %d"
-#if defined _WIN32
-#else
-			, errno
-#endif
-			);
+    if (setsockopt (sock, IPPROTO_IP, IP_HDRINCL,&flagOne,sizeof (flagOne)) == -1) {
+        fprintf (stderr, "setsockopt() failed with error: %d", errno);
         return -1;
     }
-
     // Send the packet using sendto() for sending datagrams.
-    if (sendto (sock, packet, IP4_HDRLEN + ICMP_HDRLEN + datalen, 0, (struct sockaddr *) &dest_in, sizeof (dest_in)) == -1)  
-    {
-        fprintf (stderr, "sendto() failed with error: %d"
-#if defined _WIN32
-#else
-			, errno
-#endif
-			);
+    if (sendto (sock, packet, ICMP_HDRLEN + datalen, 0, (struct sockaddr *) &dest_in, sizeof (dest_in)) == -1) {
+        fprintf (stderr, "sendto() failed with error: %d", errno);
         return -1;
     }
-  // Close the raw socket descriptor.
-#if defined _WIN32
-#else
+    // Close the raw socket descriptor.
   close(sock);
-#endif
   return 0;
 }
 
@@ -249,4 +125,3 @@ unsigned short calculate_checksum(unsigned short * paddress, int len)
 	answer = ~sum;                      // truncate to 16 bits
 	return answer;
 }
-
